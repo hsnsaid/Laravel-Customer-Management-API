c5be836fd6134e4f427e4a19b2c56d818fe8516d
isaak kbr <chrisisaak22@gmail.com>
2025-03-10 23:40:51 +0100

    Remove UI related methods from InvoiceController

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -27,14 +27,6 @@ class InvoiceController extends Controller
 
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreInvoiceRequest $request)
@@ -60,14 +52,6 @@ class InvoiceController extends Controller
     public function show(Invoice $invoice)
     {
         return new InvoiceResource($invoice);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Invoice $invoice)
-    {
-        //
     }
 
     /**
